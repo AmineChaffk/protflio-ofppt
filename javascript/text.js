032f175910e10d639911404ad32cd448d231c302
AmineChaffk <chaffakamine263@gmail.com>
2025-09-08 17:11:50 +0100

    Add files via upload

---
+++ javascript/text.js
@@ -0,0 +1,36 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        keyframes: {
+          typing: {
+            "0%": {
+              width: "0%",
+              visibility: "hidden"
+            },
+            "100%": {
+              width: "100%"
+            }
+          },
+          blink: {
+            "50%": {
+              borderColor: "transparent"
+            },
+            "100%": {
+              borderColor: "white"
+            }
+          }
+        },
+        animation: {
+          typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+        }
+      },
+    },
+    plugins: [],
+}
+document.getElementById("button").addEventListener("click", function(){
+    document.getElementById("header").style.display = "block";
+})
+document.getElementById("x").addEventListener("click", function(){
+    document.getElementById("header").style.display = "none";
+})
